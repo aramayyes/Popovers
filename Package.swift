723c9c7f8e94962f1f0b8bfbaf5adaa8211848fd
Aramayis <aramayis.amiraghyan@outlook.com>
2023-06-05 12:12:08 +0400

    Version 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Popovers",
     platforms: [
-        .iOS("14.0"),
+        .iOS("13.0"),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
