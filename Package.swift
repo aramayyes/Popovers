592e7838653a9ccb638e8732dd5be1c846f046f1
Aramayis <aramayis.amiraghyan@outlook.com>
2023-06-05 12:59:22 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -28,7 +28,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Popovers",
-            dependencies: ["Introspect"],
+            dependencies: [.product(
+                name: "Introspect",
+                package: "SwiftUI-Introspect"
+            )],
             path: "Sources"
         ),
     ]
